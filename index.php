a8f165d49bc3a419712e3a3e24a0627934f6775e
fathur <fathur_rohman17@yahoo.co.id>
2014-10-03 06:09:30 +0700

    desain kasar

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>		<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>			<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>			<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->	<html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Algoon</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+			
+		<link rel="stylesheet" href="packages/bootstrap/css/bootstrap.min.css">
+		
+		<link rel="stylesheet" href="packages/bootstrap/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="styles/main.css">
+
+		<script src="scripts/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+		<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		<![endif]-->
+
+		<nav class="sidemenu">
+			<ul class="nav nav-pills">
+				<li>d</li>
+				<li>d</li>
+			</ul>
+		</nav>
+		
+		<div class="container-fluid movies">
+			<div class="row">
+				<?php for ($i=0; $i < 12; $i++) { ?>
+				<div class="col-sm-3 col-md-2 col-lg-2 item">
+					<img src="images/box.png" class="img-responsive">
+				</div>
+				<?php }?>
+			</div>
+		</div>
+
+
+		<script src="scripts/jquery-1.11.1.min.js"></script>
+		<script>window.jQuery || document.write('<script src="scripts/jquery-1.11.1.min.js"><\/script>')</script>
+
+		<script src="packages/bootstrap/js/bootstrap.min.js"></script>
+
+		<script src="scripts/plugins.js"></script>
+		<script src="scripts/main.js"></script>
+	</body>
+</html>
