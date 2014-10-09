62454c1780703bafeab62e5738a51050f813ee72
fathur <fathur_rohman17@yahoo.co.id>
2014-10-09 22:26:04 +0700

    remove unused pagination

--- front-page.php
+++ front-page.php
@@ -64,15 +64,6 @@ get_header(); ?>
 
 <div class="paging">
 	<?php wp_pagenavi(); ?>
-	<!-- <ul class="pagination">
-		<li class="disabled"><a href="#">Previous</a></li>
-		<li class="active"><a href="#">1</a></li>
-		<li><a href="#">2</a></li>
-		<li><a href="#">3</a></li>
-		<li><a href="#">4</a></li>
-		<li><a href="#">5</a></li>
-		<li><a href="#">Next</a></li>
-	</ul> -->
 </div>
 
 <?php get_footer(); ?>
