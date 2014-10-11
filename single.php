c4e031f7c8bfba8ce8c37b92686a5e3937e6fe5a
fathur <fathur_rohman17@yahoo.co.id>
2014-10-11 10:32:38 +0700

    add title to single

--- single.php
+++ single.php
@@ -16,6 +16,12 @@ get_header(); ?>
 		// Start the Loop.
 		while ( have_posts() ) : the_post();
 
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+			endif;
+
 			the_content();
 
 			
